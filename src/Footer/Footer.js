4bc2d32e9d7651495d1de320c991a3bab0bbf647
SaiKumar <skodumun@uncc.edu>
2023-10-01 20:44:52 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+      <div className="center">
+        <p>Edited by Sai Kumar</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
