1d4f3a241bdb6e60eb2acd6f283a31487782b544
SaiKumar <skodumun@uncc.edu>
2023-10-01 21:06:55 -0400

    Added Instructions

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bottom">
-      <div className="center">
+      <div className="center" >
         <p>Edited by Sai Kumar</p>
       </div>
     </footer>
